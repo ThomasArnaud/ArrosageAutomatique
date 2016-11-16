9f26ebcb3ef29c24f5a3bded4e7d8748094247d9
ThomasArnaud <thomas.arnaud@etu.univ-lyon1.fr>
2016-11-16 16:31:31 +0100

    fix lamp controler

--- public/processes/process-lamp.php
+++ public/processes/process-lamp.php
@@ -1,8 +1,8 @@
 <?php
     if($_POST["update"]) {
-        touch(__DIR__ . "/../../data/" . "valve" . $_POST["zone"]);
+        touch(__DIR__ . "/../../data/lamp" . $_POST["zone"] . "_off.txt");
     } else {
-        touch(__DIR__ . "/../../data/" . "valve" . $_POST["zone"]);
+        touch(__DIR__ . "/../../data/lamp" . $_POST["zone"] . "_off.txt");
     }
 
 header("location: ../");
