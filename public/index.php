ccc18801234839ac59fb665161976a7725391045
Bruno Buiret <bruno.buiret@gmail.com>
2016-11-20 18:02:07 +0100

    Bonnes pratiques

--- public/index.php
+++ public/index.php
@@ -1,66 +1,87 @@
 <?php session_start(); ?>
-
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Automated Watering</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <style>body { padding-top: 70px; }</style>
-</head>
-
-<body>
-    <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="/index.php">Automate d'arrosage</a>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="author" content="Thomas ARNAUD, Bruno BUIRET, Alexis RABILLOUD" />
+        <title>Automated Watering</title>
+        <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css" media="screen" />
+        <style type="text/css">
+            body {
+                padding-top: 70px;
+            }
+        </style>
+    </head>
+    <body>
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="/index.php">Automate d'arrosage</a>
+                </div>
             </div>
-        </div>
-    </nav>
-
-    <?php
-        if (file_exists(__DIR__ . "/../data/automaton.ini")) {
-            $ini = parse_ini_file(__DIR__ . "/../data/automaton.ini", true);
-            for( $i = 1; $i <= $ini["settings"]["zones_number"]; $i++ ) { ?>
-                <div class="container">
+        </nav>
+        <div class="container">
+            <?php if (file_exists(__DIR__ . '/../data/automaton.ini')):
+                $ini = parse_ini_file(__DIR__ . '/../data/automaton.ini', true);
+                for( $i = 1; $i <= $ini['settings']['zones_number']; $i++ ): ?>
                     <div class="panel panel-default">
-                        <div class="panel-heading">Zone <?php echo($i); ?></div>
+                        <div class="panel-heading">
+                            Zone <?php echo($i); ?>
+                        </div>
                         <div class="panel-body">
-                            <?php if(!$ini["valves"]["valve_".$i]) {?>
-                            <form method="POST" class="form-inline" action="processes/process-watering.php">
-                                <div class="form-group <?php if(isset($_SESSION['err' . $i]) && $_SESSION['err' . $i]) {echo("has-error");} ?>" >
-                                    <input type="hidden" name ="zone" value="<?php echo($i); ?>">
-                                    <label for="<?php echo($i); ?>">Arrosage</label>
-                                    <input name="update" type="text" class="form-control" id="<?php echo($i); ?>">
-                                </div>
-                                <button type="submit" class="btn btn-success">
-                                    Allumer
-                                </button>
-                                <?php if(isset($_SESSION['err' . $i]) && $_SESSION['err' . $i]) { ?>
-                                    <span class="help-block">La quantité d'eau pour l'arrosage doit être entière et inférieure à la capacité de la cuve.</span>
-                                <?php } ?>
-                            </form>
-                            <?php } else {?>
-                                <p>Arrosage en fonctionnement</p>
-                            <?php }?>
-                            <form method="POST" class="form-inline" action="processes/process-lamp.php">
+                            <?php if(!$ini['valves']['valve_'.$i]): ?>
+                                <form method="post" class="form-inline" action="/processes/process-watering.php">
+                                    <input type="hidden" name ="zone" value="<?php echo($i); ?>" />
+                                    <div class="form-group <?php if(isset($_SESSION['err' . $i]) && $_SESSION['err' . $i]) echo('has-error'); ?>">
+                                        <label for="<?php echo($i); ?>">Arrosage</label>
+                                        <div class="input-group">
+                                            <?php if(!$ini['pump']['pump']): ?>
+                                                <input type="text" name="update" class="form-control" id="<?php echo($i); ?>" />
+                                                <span class="input-group-btn">
+                                                <button type="submit" class="btn btn-success">
+                                                    Allumer
+                                                </button>
+                                            </span>
+                                            <?php else: ?>
+                                                <input type="text" name="update" class="form-control" id="<?php echo($i); ?>" disabled="disabled" />
+                                                <span class="input-group-btn">
+                                                <button type="submit" class="btn btn-success" disabled="disabled">
+                                                    Allumer
+                                                </button>
+                                            </span>
+                                            <?php endif; ?>
+                                        </div>
+                                        <?php if(isset($_SESSION['err' . $i]) && $_SESSION['err' . $i]): ?>
+                                            <span class="help-block">La quantité d'eau pour l'arrosage doit être entière et inférieure à la capacité de la cuve.</span>
+                                        <?php endif; ?>
+                                    </div>
+                                </form>
+                            <?php else: ?>
+                                <p>Arrosage en cours</p>
+                            <?php endif; ?>
+                            <form method="post" class="form-inline" action="/processes/process-lamp.php">
+                                <input type="hidden" name ="zone" value="<?php echo($i); ?>" />
                                 <div class="form-group">
                                     <label for="lampe<?php echo($i); ?>">Éclairage</label>
-                                    <input type="hidden" name ="zone" value="<?php echo($i); ?>">
+                                    <button name ="update" type="submit" class="btn <?php echo($ini['lighting']['lamp_'.$i] ? 'btn-danger' : 'btn-success'); ?>" value="<?php echo($ini['lighting']['lamp_'.$i]); ?>">
+                                        <?php echo($ini['lighting']['lamp_'.$i] ? 'Éteindre' : 'Allumer'); ?>
+                                    </button>
                                 </div>
-                                <button name ="update" type="submit" class="btn <?php echo($ini["lighting"]["lamp_".$i] ? "btn-danger" : "btn-success"); ?>" value="<?php echo($ini["lighting"]["lamp_".$i]); ?>">
-                                    <?php echo($ini["lighting"]["lamp_".$i] ? "Éteindre" : "Allumer"); ?>
-                                </button>
                             </form>
                         </div>
                     </div>
-                </div>
-    <?php }}; ?>
-</body>
-</html>
+                <?php endfor; ?>
+            <?php else: ?>
+                <p>L'automate n'a encore été configuré.</p>
+            <?php endif; ?>
+        </div>
 
+        <script type="text/javascript" src="/js/jquery.min.js"></script>
+        <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+    </body>
+</html>
+<?php
+// Erase errors after they have been displayed.
+$_SESSION = array();
--- public/processes/process-lamp.php
+++ public/processes/process-lamp.php
@@ -1,8 +1,10 @@
 <?php
-    if($_POST["update"]) {
-        touch(__DIR__ . "/../../data/lamp" . $_POST["zone"] . "_off.txt");
-    } else {
-        touch(__DIR__ . "/../../data/lamp" . $_POST["zone"] . "_on.txt");
-    }
+session_start();
 
-header("location: ../");
+if($_POST['update']) {
+    touch(__DIR__ . '/../../data/lamp' . $_POST['zone'] . '_off.txt');
+} else {
+    touch(__DIR__ . '/../../data/lamp' . $_POST['zone'] . '_on.txt');
+}
+
+header('Location: /index.php');
